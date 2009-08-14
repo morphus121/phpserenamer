75c4874e5b521e64670a3519452c90716c45e7b5
adriengallou <adriengallou@a2a44f00-7ee7-11de-8c42-8d293cda74e2>
2009-08-14 00:20:17 +0000

    Fixes issue 17

---
+++ trunk/lib/interface/srComboBoxProviders.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *
+ * @author adriengallou
+ *
+ */
+class srComboBoxProviders extends GtkComboBox
+{
+
+	/**
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$list  = sfConfig::get('sr_providers');
+    $model = new GtkListStore(GObject::TYPE_STRING);
+    $this->set_model($model);
+    $cellRenderer = new GtkCellRendererText();
+    $this->pack_start($cellRenderer);
+    $this->set_attributes($cellRenderer, 'text', 0);
+    $model->clear();
+    foreach($list as $choice) {
+      $model->append(array($choice));
+    }
+    $this->set_active_iter($model->get_iter(0));
+
+	}
+
+	/**
+	 * Retourne le provider sélectionné
+	 *
+	 * @return string
+	 */
+  public function getSelectedProvider()
+  {
+  	return $this->get_active_text();
+  }
+
+}
