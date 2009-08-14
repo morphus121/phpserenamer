70edb909a2ddad7a0288a2efb730cbeedab36833
adriengallou <adriengallou@a2a44f00-7ee7-11de-8c42-8d293cda74e2>
2009-08-14 12:08:50 +0000

    Fixes issue 19

--- lib/util/myWebBrowser.class.php
+++ lib/util/myWebBrowser.class.php
@@ -1,50 +1,35 @@
 <?php
+/**
+ *
+ * @author adriengallou
+ *
+ */
 class myWebBrowser extends sfWebBrowser
 {
 
-  protected $cache_expiration = 2592000; // 30 days default cache
-
-//  /**
-//   * return current cache expiration in seconds
-//   *
-//   * @return integer
-//   */
-//  public function getCacheExpiration() {
-//    return $this->cache_expiration;
-//  }
-//
-//  /**
-//   * set cache expiration
-//   *
-//   * @param integer $seconds
-//   * @return integer
-//   */
-//  public function setCacheExpiration($seconds) {
-//    $this->cache = $seconds;
-//    return $this->cache_expiration;
-//  }
+  const EXPIRATION = 2592000; //30 jours
 
   /**
-   * cached get
+   * Get mettant les pages en cache
    *
-   * @param string The request uri
-   * @param array  The request parameters (associative array)
-   * @param array  The request headers (associative array)
+   * @param string url
+   * @param array  paramètres de la requete
+   * @param array  headers
    *
-   * @return sfWebBrowser The current browser object
+   * @return sfWebBrowser
    */
   public function get($uri, $parameters = array(), $headers = array())
   {
-    $c = new sfFileCache(array('cache_dir' => sfConfig::get('sf_cache_dir')));
+    $fc          = new sfFileCache(array('cache_dir' => sfConfig::get('sf_cache_dir')));
     $encoded_uri = urlencode($uri);
-    $clef = $encoded_uri . 'myWebBrowser';
-    if($c->has($clef) && (time() < ($this->cache_expiration + $c->getLastModified($clef))))
+    $clef        = sprintf('%smyWebBrowser', $encoded_uri);
+
+    if($fc->has($clef) && (time() < (self::EXPIRATION + $fc->getLastModified($clef))))
     {
       $this->setResponseCode(200);
       $this->setResponseHeaders();
-      $this->setResponseText($c->get($clef));
+      $this->setResponseText($fc->get($clef));
 
-      //TODO pourquoi ca ne marche pas en parssant par le getResponseDom ?
       $this->responseDom = new DomDocument('1.0', 'utf8');
       $this->responseDom->validateOnParse = true;
       @$this->responseDom->loadHTML($this->getResponseText());
@@ -54,7 +39,7 @@ class myWebBrowser extends sfWebBrowser
     else
     {
       $ret = parent::get($uri, $parameters, $headers);
-      $c->set($clef, $this->getResponseText());
+      $fc->set($clef, $this->getResponseText());
       return $ret;
     }
   }
