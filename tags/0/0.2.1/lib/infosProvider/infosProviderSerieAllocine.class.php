de93a2761492abab35590c76d78b4cebfa7479a8
adriengallou <adriengallou@a2a44f00-7ee7-11de-8c42-8d293cda74e2>
2009-09-17 20:25:50 +0000

    Tag de la 0.2.1 (Fixes issue 72)

---
+++ tags/0/0.2.1/lib/infosProvider/infosProviderSerieAllocine.class.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ *
+ * @author adriengallou
+ *
+ */
+class infosProviderSerieAllocine extends infosProviderSerieBase
+{
+
+  /**
+   * (non-PHPdoc)
+   * @see lib/infosProvider/infosProviderSerieBase#getSeries($serie)
+   */
+  public function getSeries($serie)
+  {
+    $serie = str_replace('.', ' ', $serie);
+
+    if(!count($tab = $this->getTableauIdSerie($serie)))
+    {
+      throw new SerieNonFoundException();
+    }
+    $series = array();
+    foreach($tab as $val)
+    {
+      $series[] = $val['nomTrouve'];
+    }
+    return $series;
+  }
+
+  /**
+   * (non-PHPdoc)
+   * @see lib/infosProvider/infosProviderSerieBase#getEpisode($serie, $saison, $episode)
+   */
+  public function getEpisode($serie, $saison, $episode)
+  {
+    $tableau = $this->getTableauIdSerie($serie);
+
+    $id = null;
+    foreach($tableau as $tab)
+    {
+      if($tab['nomTrouve'] == $serie)
+      {
+        $id = $tab['idTrouve'];
+      }
+    }
+
+    if(is_null($id))
+    {
+      throw new SerieNonFoundException();
+    }
+
+    $this->browser->get($this->getLienSaison($id, $saison));
+
+    $oDomDocument = $this->browser->getResponseDom();
+    $xpath = new DOMXPath($oDomDocument);
+
+    $query = '//h4';
+    $oDomNodeList = $xpath->query($query);
+
+    if($oDomNodeList->length == 0)
+    {
+      throw new EpisodeNonFoundException();
+    }
+
+    //On crée un tableau episode -> serie
+    $tab = array();
+    for($i=0; $i <= $oDomNodeList->length;$i++)
+    {
+      $oDomNode = $oDomNodeList->item($i);
+
+      if(!is_null($oDomNode->attributes) )
+      {
+        $countAttributes = 0;
+        foreach ($oDomNode->attributes as $attrName => $attrNode)
+        {
+          $countAttributes++;
+        }
+        if($countAttributes)
+        {
+          if(in_array($oDomNode->attributes->getNamedItem('style')->nodeValue, array('color:#000000', 'color:gray')))
+          {
+            $tab[] = $oDomNode->nodeValue;
+          }
+        }
+      }
+    }
+
+    $episodes = array();
+    for($i=0;$i<count($tab);$i = $i + 2)
+    {
+      $num = trim(substr($tab[$i], 8));
+      $unEpisode = trim($tab[$i + 1]);
+      $episodes[$num] = $unEpisode;
+    }
+
+    if(!array_key_exists($episode, $episodes))
+    {
+      throw new EpisodeNonFoundException();
+    }
+
+    return $episodes[$episode];
+
+  }
+
+//TODO lien saison en cours ?
+  private function getLienSaison($numeroAllocineSerie, $numSaison)
+  {
+    $this->browser->get(sprintf('http://www.allocine.fr/series/episodes_gen_cserie=%s.html', $numeroAllocineSerie));
+
+    $oDomDocument = $this->browser->getResponseDom();
+    $xpath = new DOMXPath($oDomDocument);
+
+    $query = '//a';
+    $oDomNodeList = $xpath->query($query);
+    if($oDomNodeList->length == 0)
+    {
+      throw new SerieNonFoundException();
+    }
+
+    for($i=0; $i <= $oDomNodeList->length;$i++)
+    {
+      $oDomNode = $oDomNodeList->item($i);
+
+      if($oDomNode->nodeValue == sprintf('Saison %s', $numSaison))
+      {
+        return sprintf('http://www.allocine.fr%s', $oDomNode->attributes->getNamedItem('href')->nodeValue);
+      }
+    }
+    throw new SerieNonFoundException();
+  }
+
+  /**
+   * Renvoi un tableau de type :
+   *  array[] => array(
+   *    'nomCherche' => 'nomRecherche',
+   *    'idTrouve'   => 'idTrouve',
+   *    'nomTrouve'  => 'nomTrouve'
+   *  )
+   *  ...
+   *
+   * @param  $serie
+   * @return array
+   */
+  private function getTableauIdSerie($serie)
+  {
+    $this->get($this->rechercheParTitre($serie));
+
+    $oDomDocument = $this->browser->getResponseDom();
+    $xpath = new DOMXPath($oDomDocument);
+
+    $query = '//h4/a';
+    $oDomNodeList = $xpath->query($query);
+    if($oDomNodeList->length == 0)
+    {
+      throw new SerieNonFoundException();
+    }
+
+    for($i=0; $i <= $oDomNodeList->length;$i++)
+    {
+      $oDomNode = $oDomNodeList->item($i);
+
+      if(!is_null($oDomNode->nodeValue) && $oDomNode->nodeValue != 'Et la réponse est...')
+      {
+        $matches = array();
+        if(preg_match('/\/series\/ficheserie_gen_cserie=(.*)\.html/',$oDomNode->attributes->getNamedItem('href')->nodeValue, $matches))
+        {
+          $liste[] = array(
+            'nomCherche' => $serie,
+            'nomTrouve'  => $oDomNode->nodeValue,
+            'idTrouve'   => $matches[1]
+          );
+        }
+      }
+    }
+
+    return $liste;
+  }
+
+  public function nettoyerNomSerie($serie)
+  {
+    return parent::nettoyerNomSerie($serie);
+  }
+
+  /**
+   * Retourne l'url de recherche par titre
+   */
+  private function rechercheParTitre($titre)
+  {
+    return sprintf('http://www.allocine.fr/recherche/?motcle=%s&x=0&y=0&rub=6', urlencode($titre));
+  }
+}
---
+++ tags/0/0.2.1/lib/util/srutils.class.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ *
+ * @author adriengallou
+ *
+ */
+class srUtils
+{
+
+  const VERSION = '0.2.1';
+
+  /**
+   * Retourne le numéro de version de l'application
+   *
+   * @return string
+   */
+  public static function getVersion()
+  {
+    return self::VERSION;
+  }
+
+  /**
+   * Enlève d'une chaine tout les caractères interdits dans un nom de fichier
+   *
+   * @param  string $nom
+   * @return string
+   */
+  public static function nameForFileSystem($nom)
+  {
+    return str_replace(
+    array("/", 'ñ'),
+    array("-", 'n'),
+    str_replace(array(':', '?'), '', $nom)
+    );
+  }
+
+  /**
+   * Retourne la liste des providers en fonction des classes présentes dans le
+   * dossier lib
+   *
+   * @return array[] => string
+   */
+  public static function getProvidersFromClassesNames()
+  {
+    $files = sfFinder::type('file')
+    ->prune('plugins')
+    ->prune('vendor')
+    ->prune('skeleton')
+    ->prune('default')
+    ->name('infosProviderSerie*\.class\.php')
+    ->in(sfConfig::get('sf_lib_dir'))
+    ;
+    $providers = array();
+    foreach($files as $filePath)
+    {
+      $matches = array();
+      if(preg_match('/infosProviderSerie(.*)\.class/', ($file = pathinfo($filePath, PATHINFO_FILENAME)), $matches))
+      {
+        if(($name = $matches[1]) != 'Base')
+        {
+          $providers[] = strtolower($name);
+        }
+      }
+    }
+    return $providers;
+  }
+
+  public static function flattenArray(array $array, $keyBegin = '')
+  {
+    $flat = array();
+    foreach ($array as $key => $value)
+    {
+      $laClef = ($keyBegin == '') ? $key : $keyBegin . '_' . $key;
+      if (is_array($value)) $flat = array_merge($flat, self::flattenArray($value, $laClef));
+      else $flat[$laClef] = $value;
+    }
+    return $flat;
+  }
+
+  public static function unFlattenArray(array $array)
+  {
+    $tab = array();
+    foreach($array as $clef => $valeur)
+    {
+      $tabClef  = explode('_', $clef);
+      $firstKey = array_shift($tabClef);
+      $moinsUn  = count($tabClef) - 1;
+      $tempTab  = array($tabClef[$moinsUn] => $valeur);
+      for($i = $moinsUn-1; $i >= 0; $i--)
+      {
+        $tempTab = array($tabClef[$i] => $tempTab);
+      }
+      $tab[$firstKey] = (isset($tab[$firstKey])) ? array_merge_recursive($tab[$firstKey], $tempTab) : $tempTab;
+    }
+    return $tab;
+  }
+
+}
