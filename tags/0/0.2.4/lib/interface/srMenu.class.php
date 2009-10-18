45a048cf5c969bbf96c36e7d2e9ab5354a43f79f
adriengallou <adriengallou@a2a44f00-7ee7-11de-8c42-8d293cda74e2>
2009-10-18 13:15:50 +0000

    Tag de la 0.2.4 (Fixes issue 115)

---
+++ tags/0/0.2.4/lib/interface/srMenu.class.php
@@ -0,0 +1,124 @@
+<?php
+class srMenu
+{
+  //@see http://gtk.php.net/manual/en/gtk.enum.stockitems.php
+  private static $menu_definition = array(
+    '_Actions' => array(
+      array(
+        'nom'   => 'Ajouter _Dossier',
+        'image' => Gtk::STOCK_OPEN
+      ),
+      array(
+        'nom'   => 'Ajouter _Fichier',
+        'image' => Gtk::STOCK_NEW
+      ),
+      array(
+        'nom'   => '_Supprimer Element',
+        'image' => Gtk::STOCK_CLOSE
+      ),
+      array(
+        'nom'   => '_Quitter',
+        'image' => Gtk::STOCK_QUIT
+      )
+    ),
+    '_Edition' => array(
+      array(
+        'nom'   => '_Paramètres',
+        'image' => Gtk::STOCK_PROPERTIES
+      )
+    ),
+    'Aide'    => array(
+      array(
+        'nom'   => 'A propos',
+        'image' => Gtk::STOCK_DIALOG_QUESTION,
+      )
+    ),
+  );
+
+  public static function getInstance()
+  {
+    return self::setup_menu(self::$menu_definition);
+  }
+
+  private function setup_menu($menus)
+  {
+    $menubar = new GtkMenuBar();
+    foreach($menus as $toplevel => $sublevels)
+    {
+      $menubar->append($top_menu = new GtkMenuItem($toplevel));
+      $menu = new GtkMenu();
+      $top_menu->set_submenu($menu);
+      foreach($sublevels as $submenu)
+      {
+        if ($submenu=='<hr>')
+        {
+          $menu->append(new GtkSeparatorMenuItem());
+        }
+        else
+        {
+          if(is_array($submenu))
+          {
+            $menu_item = new GtkImageMenuItem($submenu['nom']);
+            $menu_item->set_image(GtkImage::new_from_stock($submenu['image'], Gtk::ICON_SIZE_MENU));
+          }
+          else
+          {
+            $menu_item = new GtkMenuItem($submenu);
+          }
+          $menu->append($menu_item);
+          $menu_item->connect('activate', array('srMenu','on_menu_select'));
+        }
+      }
+    }
+    return $menubar;
+  }
+
+  // process radio menu selection
+  public function on_toggle($radio)
+  {
+    $label = $radio->child->get_label();
+    $active = $radio->get_active();
+    echo("radio menu selected: $label\n");
+  }
+
+  // process menu item selection
+  function on_menu_select($menu_item)
+  {
+    $item = $menu_item->child->get_label();
+    switch($item)
+    {
+      case 'Ajouter _Dossier':
+        srWindow::clicOuvrirDossier();
+        break;
+      case 'Ajouter _Fichier':
+        srWindow::clicOuvrirFichier();
+        break;
+      case '_Supprimer Element':
+        break;
+      case '_Quitter':
+        Gtk::main_quit();
+        break;
+      case '_Paramètres':
+        $p = srParametres::getInstance();
+        $p->show_all();
+        break;
+      case 'A propos':
+        $dlg = new GtkAboutDialog();
+        $dlg->set_name('phpserenamer');
+        $dlg->set_version(srUtils::getVersion());
+        $dlg->set_comments('Nom de la série : %n
+ Numéro de la saison : %s
+ Numéro de la saison avec ajout d\'un zero devant si < 10 : %j
+ Numéro de l\'episode : %e
+ Numéro de l\'episode avec ajout d\'un zero devant si < 10 : %k
+ Nom de l\'episode : %t"');
+        $dlg->set_license("GPL v2");//Button
+        $dlg->set_website('http://code.google.com/p/phpserenamer/');
+        $dlg->set_icon_from_file(sfConfig::get('sr_logo'));
+        $dlg->run();
+        $dlg->destroy();
+        break;
+    }
+  }
+
+}
