aa683d8b68b1039977938e1a993429670ebc3819
adriengallou <adriengallou@a2a44f00-7ee7-11de-8c42-8d293cda74e2>
2009-11-22 18:32:25 +0000

    Fixes issue 108

--- trunk/lib/util/srConfig.class.php
+++ trunk/lib/util/srConfig.class.php
@@ -2,15 +2,52 @@
 class srConfig
 {
 
+  /**
+   *
+   * @var string
+   */
   private static $fichier = 'config/project.yml';
 
+  /**
+   *
+   * @var array
+   */
+  private static $config;
+
+  /**
+   *
+   * @var int
+   */
+  private static $lastModification = 0;
+
+  /**
+   *
+   * @return array
+   */
+  private static function getConfig($force = false)
+  {
+    $modification = filemtime(self::$fichier);
+    if($modification != self::$lastModification || $force)
+    {
+      $array = sfYaml::load(self::$fichier);
+      if(!is_null($array))
+      {
+        $array = srUtils::flattenArray($array);
+      }
+      self::$config           = $array;
+      self::$lastModification = $modification;
+    }
+    return self::$config;
+  }
+
+  /**
+   *
+   * @param  string $clef
+   * @return string
+   */
   public function get($clef)
   {
-    $array = sfYaml::load(self::$fichier);
-    if(!is_null($array))
-    {
-      $array = srUtils::flattenArray($array);
-    }
+    $array = self::getConfig();
     $value = (array_key_exists('all_' . $clef, $array)) ? $array['all_' . $clef] : false;
 
     switch($clef)
@@ -40,22 +77,17 @@ class srConfig
 
   /**
    *
-   * @param $clef (sans le all)
-   * @param $valeur
-   * @return unknown_type
+   * @param  string $clef (sans le all)
+   * @param  mixed  $valeur
+   * @return mixed
    */
   public function set($clef, $valeur)
   {
-    $array = sfYaml::load(self::$fichier);
-    if(!is_null($array))
-    {
-      $array = srUtils::flattenArray($array);
-    }
+    $array = self::getConfig(true);
     $array['all_' . $clef] = $valeur;
     $array = srUtils::unFlattenArray($array);
     $fp = fopen(sfConfig::get('sr_root_dir') . self::$fichier, 'w');
     fwrite($fp, sfYaml::dump($array, 4));
   }
-
 
 }
