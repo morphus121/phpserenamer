b707bc158e0716a1d01d7ed0a197702c65c4a865
adriengallou <adriengallou@a2a44f00-7ee7-11de-8c42-8d293cda74e2>
2009-12-20 12:55:49 +0000

    Update issue 154
    add provider for thetvdb

---
+++ lib/infosProvider/infosProviderSerieThetvdben.class.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Classe infosProviderSerieThetvdben
+ *
+ * PHP version 5
+ *
+ * @package Interface
+ * @author  Adrien Gallou <adriengallou@gmail.com>
+ * @version SVN: <svn_id>
+ */
+
+/**
+ * infosProviderSerieThetvdben
+ *
+ * @package InfosProvider
+ * @author  Adrien Gallou <adriengallou@gmail.com>
+ * @version Release: <package_version>
+ *
+ */
+class infosProviderSerieThetvdben extends infosProviderSerieThetvdbBase
+{
+
+  /**
+   * (non-PHPdoc)
+   *
+   * @see lib/infosProvider/infosProviderSerieThetvdbBase#getLanguage()
+   *
+   * @return string
+   */
+  protected function getLanguage()
+  {
+    return 'en';
+  }
+}
