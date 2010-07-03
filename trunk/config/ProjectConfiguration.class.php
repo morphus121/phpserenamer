64810bf631994b6c893409e0a77d28b5688703f9
adriengallou <adriengallou@a2a44f00-7ee7-11de-8c42-8d293cda74e2>
2010-07-03 16:44:03 +0000

    Fixes issue 110
     * specify plugins to use on not those to disable.

--- trunk/config/ProjectConfiguration.class.php
+++ trunk/config/ProjectConfiguration.class.php
@@ -7,10 +7,14 @@ sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
+  protected $plugins = array();
+
   public function setup()
   {
     // for compatibility / remove and enable only the plugins you want
-    $this->enableAllPluginsExcept(array('sfDoctrinePlugin', 'sfCompat10Plugin', 'sfPropelPlugin'));
+    //$this->enableAllPluginsExcept(array('sfDoctrinePlugin', 'sfCompat10Plugin', 'sfPropelPlugin'));
+
+    $this->plugins = array('sfWebBrowserPlugin');
     ini_set("php-gtk.codepage", "UTF-8");
     $this->loadProjectConfig();
   }
@@ -23,6 +27,14 @@ class ProjectConfiguration extends sfProjectConfiguration
       require $this->getConfigCache()->checkConfig('config/project.yml');
       $load = true;
     }
+  }
+
+  public function getAllPluginPaths()
+  {
+    $tab = array(
+      'sfWebBrowserPlugin' => sfConfig::get('sf_plugins_dir') . DIRECTORY_SEPARATOR . 'sfWebBrowserPlugin',
+    );
+    return $tab;
   }
 
 }
